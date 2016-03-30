2c309c0314272c9f2d6723b8f3e91cb1cd758ecf
Vaibhav Sharma <vaibhav@umn.edu>
2016-03-29 23:09:05 -0500

    Committing the type coercion rules for allowing upcasts from int to float in assignments and in binary operations

--- Assig3/symboltable.c
+++ Assig3/symboltable.c
@@ -55,8 +55,9 @@ void delete_scope(int scope) {
     void **list = ht_to_list(symtab,&sz);
     for (i=0; i<sz; i++) {
         symtab_entry *entry = (symtab_entry *) list[i];
+	if(entry) debug("symboltable::delete_scope about to delete %s(%d)",entry->name,entry->scope);
         if (entry && entry->scope >= scope) {
-	  debug("symboltable::delete_scope deleting %s(%d)",entry->name,entry->scope);
+	  debug("symboltable::delete_scope deleting %s(%d)",entry->name, entry->scope);
             h_remove(symtab, gen_key(entry->name, scope));
         }
     }
