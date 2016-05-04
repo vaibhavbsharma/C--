34bf1abd97c59c0476835023837ce65ddb827272
Vaibhav Sharma <vaibhav@umn.edu>
2016-05-04 13:05:25 -0500

    Adding new unneeded stores to check compiler pass correctness

--- Assig5/test.c
+++ Assig5/test.c
@@ -4,8 +4,9 @@ int global;
 
 int main() {
   int i=0, j=0, k=0;
+  global=i; //Store removed by StoresRemoval compiler pass
+  global=j; //Store removed by StoresRemoval compiler pass
   global=i;
-  global=j;
   if(i>0) printf("greater\n");
   return global;
 }
